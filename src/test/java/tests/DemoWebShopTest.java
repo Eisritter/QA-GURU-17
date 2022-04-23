3f4a2b7e5987b306abd2d07dc16b0cbe73f270cb
roman.vorotnyak <Roman.Vorotnjak_ext@leroymerlin.ru>
2022-04-23 14:13:33 +0300

    add homework №19

--- src/test/java/tests/DemoWebShopTest.java
+++ src/test/java/tests/DemoWebShopTest.java
@@ -1,19 +1,31 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static listenters.CustomAllureListener.withCustomTemplates;
 import static org.hamcrest.Matchers.is;
 
 public class DemoWebShopTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.baseURI = "http://demowebshop.tricentis.com/";
+    }
+
+    @Tag("FirstTest")
     @Test
-    void addToCart() {
+    void addToCartWithAllureListener() {
         given()
+                .filter(new AllureRestAssured())
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .body("product_attribute_16_5_4=14&product_attribute_16_6_5=17&" +
                         "product_attribute_16_3_6=18&product_attribute_16_4_7=20&addtocart_16.EnteredQuantity=1")
                 .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/16/1")
+                .post("/addproducttocart/details/16/1")
                 .then()
                 .log().all()
                 .statusCode(200)
@@ -24,12 +36,13 @@ public class DemoWebShopTest {
     }
 
     @Test
-    void subscribeNewsLetter() {
+    void subscribeNewsLetterWithCustomFilters() {
         given()
+                .filter(withCustomTemplates())
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .body("email=test@test.com")
                 .when()
-                .post("http://demowebshop.tricentis.com/subscribenewsletter")
+                .post("/subscribenewsletter")
                 .then()
                 .log().all()
                 .statusCode(200)
